db84d791499432c9b335c2fd70a356b355c4e9e6
Idanturjeman <idan_turjeman29@walla.co.il>
2022-03-07 19:03:24 +0200

    test's changes

--- Test.cpp
+++ Test.cpp
@@ -5,21 +5,11 @@ using namespace ariel;
 using namespace std;
 
 
-<<<<<<< HEAD
 
 
 TEST_CASE("Bad mat code"){
     CHECK_THROWS(mat(4,3,'@','-'));
     CHECK_THROWS(mat(7,2,'@','-'));
-=======
-TEST_CASE("Bad mat code"){
-    CHECK_THROWS(mat(4,3,'@','-'));
-    CHECK_THROWS(mat(7,2,'@','-'));
-    //CHECK_THROWS(mat(5,3,1,'-'));
-    //CHECK_THROWS(mat(4.5,3,'@',1));
-    //CHECK_THROWS(mat('@',3,'@','-'));
-    //CHECK_THROWS(mat(5,'-','@','-'));
->>>>>>> c57797928e12bd1f5c36c8b55ad9057df404a35a
     CHECK_THROWS(mat(0,0,'@','-'));
     CHECK_THROWS(mat(0,3,'@','-'));
     CHECK_THROWS(mat(5,0,'@','-'));
@@ -29,7 +19,6 @@ TEST_CASE("Bad mat code"){
     CHECK_THROWS(mat(2,8,'@','-'));
     CHECK_THROWS(mat(4,3,'@','-'));
     CHECK_THROWS(mat(5,2,'@','-'));
-<<<<<<< HEAD
    /* CHECK_THROWS(mat(5,0,'@','-'));
     CHECK_THROWS(mat(-7,2,'@','-'));
     CHECK_THROWS(mat(0,12,'@','-'));
@@ -42,9 +31,6 @@ TEST_CASE("Bad mat code"){
     CHECK_THROWS(mat(6,3,'@','-'));
     CHECK_THROWS(mat(5,4,'@','-'));
     */
-=======
-    
->>>>>>> c57797928e12bd1f5c36c8b55ad9057df404a35a
 
 
 }
@@ -65,7 +51,6 @@ TEST_CASE("Good mat code"){
     CHECK(mat(11,7,'@','-') == string("@@@@@@@@@@@\n  @---------@\n @-@@@@@@@-@\n @-@-----@-@\n @-@@@@@@@-@\n @---------@\n @@@@@@@@@@@"));                               
     
     CHECK(mat(1,1,'$','#') == string("$"));
-<<<<<<< HEAD
     
     
     CHECK(mat(3,5,'@','@') == string("@@@\n @@@\n @@@\n @@@\n @@@")); 
@@ -83,9 +68,6 @@ TEST_CASE("Good mat code"){
 
     CHECK(mat(5,3,'-','@') == string("-----\n -@@@-\n  -----")); 
 */
-=======
-
->>>>>>> c57797928e12bd1f5c36c8b55ad9057df404a35a
 
 
 }
