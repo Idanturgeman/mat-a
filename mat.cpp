7ccd7ca4a7a66c0b7a3935630c9027dddf9e1b82
Idanturjeman <idan_turjeman29@walla.co.il>
2022-03-07 18:53:56 +0200

    first commit

---
+++ mat.cpp
@@ -0,0 +1,33 @@
+#include<stdexcept>
+#include "mat.hpp"
+
+using namespace std;
+
+namespace ariel{
+
+    string mat(int col, int row, char a, char b){
+         /*char ** mat = NULL;
+        mat = new char*[row];
+        for (size_t k = 0; k < row; k++)
+        {
+            mat[k] = new char[col];
+        }
+        
+        for (size_t i = 0; i < row; i++)
+        {
+            for (size_t j = 0; j < col; j++)
+            {
+               mat[i][j] = min(i,j);
+            }
+            
+             code 
+        }*/
+        
+        return "";
+        
+    }
+
+
+
+
+}
---
+++ mat.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#include<string>
+
+
+namespace ariel {
+
+    std::string mat(int col, int row, char symbol1, char symbol2);
+
+
+
+
+
+
+
+
+
+
+}
